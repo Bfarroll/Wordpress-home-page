58a53a95947f6b430a680c45c1ba5d47ed7d6d12
Billy Farroll <watanight@hotmail.co.uk>
2016-02-01 17:21:02 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,91 @@
+[full_width_section bg_pos="Center Center" bg_repeat="No-Repeat" text_color="Light" image_url="http://griffinclub.com/moneybackmortgages/wp-content/uploads/2016/01/hero-2.jpg" top_padding="200" bottom_padding="200" background_color=""] 
+
+<div class="homeheader" style="width: 100%; margin-left: auto; margin-right: auto; text-align: center;">
+<h1 class="header1commission" style="color: white; width: 100%; font-size: 50px; padding: 20px; background-color: rgba(35, 33, 33, 0.7); margin-bottom: 15px;">Up to 50% Commission Comeback</h1>
+
+[button color="Accent-Color" size="medium" url="http://griffinclub.com/moneybackmortgages/index.php/what-do-i-get-back/" style="font-size: 13px; font-weight: bold; font-family: inherit;" text="calculate how much you'll get back"]
+
+[button color="Accent-Color" size="medium" url="http://griffinclub.com/moneybackmortgages/index.php/request-a-mortgage-quote/" text="Request a mortgage quote "]
+</div>
+[/full_width_section]
+
+
+[full_width_section bg_pos="Center Center" bg_repeat="No-Repeat" text_color="Light" image_url="" top_padding="40" bottom_padding="40" background_color="#0098bf"]
+<h1 class="frame title" style=" text-align: center; font-size: 30px; font-weight: bold; color: white; margin-bottom: 30px;">Money Back Mortgages Best Buys</h1> 
+<div class="frame1" style=" width: 100%; margin: auto; text-align: center;">
+<iframe src="https://crm.trigoldcrystal.co.uk/Crystal.Momentum.Web.UI.Sourcing.Mortgage/PresentationLayer/BestBuy.aspx?InstallationId=NjFmZTI4OGEtOGI4Yi00YTMyLWIyODYtMTMyZWU5ZWQzOWI2fDQyNTB8MzU0MQ==&Tabs=BTL&ShowTabs=0&Table=SUM" style=" width: 100%; opacity: 1; visibility: visible; height: 300px; background-color: white;" name="BestBuyIndividualTable" frameborder="no" allowtransparency="true">
+    Your browser does not support inline frames or is currently configured not to display inline frames.
+</iframe>
+</div>
+ [/full_width_section]
+
+
+[full_width_section bg_pos="Left Top" bg_repeat="No-Repeat" text_color="Light" image_url="" top_padding="40" bottom_padding="30" background_color="#e5e5e5"]
+<p style="text-align: center;"><span style="color: #38b9d9; font-family: tahoma, arial, helvetica, sans-serif; font-size: 18pt; font-weight: bold; ">Money Back Mortgages </span><span style="font-size: 24pt; color: black;">|</span> <span style="font-family: tahoma, arial, helvetica, sans-serif; font-size: 20pt; color: black; ">Where it pays to get advice</span> <span style="font-size: 24pt; color: black;">|</span><span style="color: #38b9d9; font-size: 18pt; font-family: tahoma, arial, helvetica, sans-serif; font-weight: bold;"> 0800 1670776</span></p>
+[/full_width_section]
+
+[full_width_section bg_pos="Left Top" bg_repeat="No-Repeat" text_color="Light" image_url="" top_padding="40" bottom_padding="40" background_color="#ffffff"][one_third boxed="true"] 
+<div class="howitworks" style="color: #0082b3; font-weight: bold; margin-bottom: 35px;">
+<span class="title" style="font-family: tahoma, arial, helvetica, sans-serif; color: #0082b3; font-size: 24pt; "><strong>How it Works</strong></span>
+</div>
+
+<div class="paragraph1" style="color: rgb(92, 94, 95); padding-right: 12px;">
+<div class="toptext" style="margin-bottom: 15px;">
+<span style="font-family: 'trebuchet ms', geneva, sans-serif; font-size: 14pt;">With so many mortgages available at any given time, it can be challenging to find the one that is the best for you and has the lowest overall cost over a set period.
+</div>
+
+<div class="middletext" style="margin-bottom: 25px;">
+<span style="font-family: 'trebuchet ms', geneva, sans-serif; font-size: 14pt;">One lender may offer a low interest rate but high arrangement fees while another will offer a higher interest rate but lower arrangement fees. With all of our recommendations we calculate the lowest overall cost for you.
+</div>
+
+<div class ="bottomtext" style="margin-bottom: 24px;">
+<span style="font-family: 'trebuchet ms', geneva, sans-serif; font-size: 14pt;">Using Money back Mortgages could not be simpler.</span>
+</div>
+</div>
+
+<div class="find button" style="background-color: #0099cc; text-align: center; width: 45%; padding-top: 10px; padding-bottom: 10px; padding-left: 1px; padding-right: 1px; ">
+<a class="maxbutton-1" style="color: white; text-align: center; font-family: Arial, Helvetica, sans-serif; font-size: 18px; cursor: pointer; font-weight: bold; "><span class="mb-text">Find Out more</span></a>
+</div>
+[/one_third]
+
+[two_thirds boxed="true"] 
+<iframe src="https://crm.trigoldcrystal.co.uk/Crystal.Momentum.Web.UI.Sourcing.Mortgage/PresentationLayer/BestBuy.aspx?InstallationId=NjFmZTI4OGEtOGI4Yi00YTMyLWIyODYtMTMyZWU5ZWQzOWI2fDQyNTB8MzU0MQ==&Tabs=PUR&ShowTabs=0&Table=TRK" style="opacity: 1; visibility: visible;width: 100%;text-align: center;margin: auto;height: 432px; margin-top: -18px;" name="BestBuyIndividualTable" width="95%" height="600px" frameborder="no" allowtransparency="true">
+    Your browser does not support inline frames or is currently configured not to display inline frames.
+</iframe> [/two_thirds][/full_width_section]
+
+
+[full_width_section bg_pos="Left Top" bg_repeat="No-Repeat" text_color="Light" image_url="http://griffinclub.com/moneybackmortgages/wp-content/uploads/2016/01/backgroundblurred.jpg" top_padding="40" bottom_padding="40" background_color=""] [one_third boxed="true"]
+<h3 class="widget-title" style="color: #0082b3; font-family: tahoma; font-size: 24px; font-weight: 5;">Testimonials</h3>
+<p class="test1" style=" color: #00131a; font-family: tahoma; font-size: 15px;">"That's great news. Wow, what a weight off my shoulders. Thank you for all your help in this matter. I will definitely be spreading the word about your great service." </p>
+<p class="name" style="color: #0082b3; font-family: tahoma; font-size: 24px; font-weight: bold; margin-bottom: -28px;">Andrew D</p>
+<br></br>
+<div class="testbutton" style="background-color: #0099cc; width: 100%; padding-top: 10px; padding-bottom: 10px; padding-left: -10px; text-align: centre; text-align: center;">
+<a class="maxbutton-3" style="color: white; text-align: center; font-family: Arial, Helvetica, sans-serif; font-size: 18px; cursor: pointer; font-weight: bold; "><span class="mb-text">See All Testimonials</span></a>
+</div> [/one_third][one_third boxed="true"]
+<h3 class="widget-title" style="color: #0082b3; font-family: tahoma; font-size: 24px; font-weight: 5;">Request a Free Mortgage Quote</h3>
+<p class="test2" style="color: #00131a; font-family: tahoma; font-size: 15px; margin-bottom: 7px; ">Arranging a mortgage through Money Back Mortgages will cost you no extra but, as we are a whole market broker you will have access to the best rates AND receive a cash-back payment on completion of your mortgage.</p>
+<p class="test">
+<div class="requestbutton" style="background-color: #0099cc;width: 100%;padding: 10px 0px 10px 10px; text-align: center;">
+<a class="maxbutton-4" style="color: white; text-align: center; font-family: Arial, Helvetica, sans-serif; font-size: 18px; cursor: pointer; font-weight: bold;"><span class="mb-text">Request Quote</span></a>
+</div>
+</p>
+[/one_third][one_third_last boxed="true"]
+<h3 class="widget-title" style="color: #0082b3; font-family: tahoma; font-size: 24px; font-weight: 5;">Up to 50% Commission Cashback</h3>
+<p class="commission-1" style="color: #00131a; font-family: tahoma; font-size: 15px; margin-bottom: 19px;">We will ensure that your mortgage completes as quickly and smoothly as possible. Once completed, we rebate up to 73% of our commission back to you.
+<br></br>
+<div class="calculatebutton" style="background-color: #0099cc; width: 100%; padding: 10px; text-align: center; margin-left: 0px;">
+<a class="maxbutton-5" style="color: white; text-align: center; font-family: Arial, Helvetica, sans-serif; font-size: 18px; cursor: pointer; font-weight: bold; margin-left: 16px;"><span class="mb-text">Calculate Cashback</span></a></p>
+</div> 
+[/one_third_last][/full_width_section]
+
+[full_width_section bg_pos="Left Top" bg_repeat="No-Repeat" text_color="Light" image_url="" top_padding="20" bottom_padding="20" background_color="#0098bf"] 
+<p class="fscsparagraph" style="text-align: center; font-weight: bold; font-size: 18px; width: 100%;">YOU ARE FULLY PROTECTED BY THE FINANCIAL SERVICES COMPENSATION SCHEME (FSCS) </p>
+
+[/full_width_section]
+
+[full_width_section bg_pos="Left Top" bg_repeat="No-Repeat" text_color="Light" image_url="" top_padding="30" bottom_padding="30" background_color="#636363"] 
+
+<p class="bottomparagraph" style="text-align: center; font-weight: bold; font-size: 16px; width: 100%; ">Think carefully before securing other debts against your home. Your home may be repossessed if you do not keep up repayments on your mortgage. Money Back Mortgages is a trading style of Mortgage Mania Ltd, which is authorised and regulated by the Financial Conduct Authority. Registered in England and Wales Company Number 7260717.</p>
+
+
+[/full_width_section] 
